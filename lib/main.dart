80e76059dea493f35bfb1ee42707ef3aa1925333
dinod tharinda <dinodtharinda2001@gmail.com>
2024-05-29 12:58:00 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
-import 'package:bloc_state_flutter/cubits_app/cubits_test_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:bloc/bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'dart:math' as math;
+
 
 void main() {
   runApp(
     const MaterialApp(
-      home: CubitsCounterScreen(),
+      home: HomePage(),
     ),
   );
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+    );
+  }
+}

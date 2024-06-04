66810130cd4cfa18a93bfa21b016e63ca8b33b6b
dinod tharinda <dinodtharinda2001@gmail.com>
2024-06-05 00:43:42 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,37 @@
+import 'package:bloc_state_flutter/features/home/ui/home.dart';
 import 'package:flutter/material.dart';
-
-
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: HomePage(),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MyApp(),
+      theme: ThemeData(
+        primaryColor: Colors.teal,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: Colors.teal,
+          iconTheme: const IconThemeData(
+            color: Colors.white,
+          ),
+          centerTitle: true,
+          titleTextStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ),
     ),
   );
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Home(),
+    );
   }
 }

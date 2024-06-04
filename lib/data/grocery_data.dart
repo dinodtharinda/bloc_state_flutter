3b81ff5f5d5ff502786e1ed2e1c46710c2f7a1c0
dinod tharinda <dinodtharinda2001@gmail.com>
2024-06-05 00:42:52 +0530

    add grocery items

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,36 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'id': '1',
+      'name': 'Bananas',
+      'description': 'Fresh bananas from Ecuador',
+      'price': 0.49,
+      'imageUrl':
+          'https://th.bing.com/th/id/OIP.4VlM4J-A3N0Eo9sUNmWXlAHaFp?rs=1&pid=ImgDetMain',
+    },
+    {
+      'id': '2',
+      'name': 'Grapes',
+      'description': 'Organic, locally grown apples',
+      'price': 1.99,
+      'imageUrl':
+          'https://th.bing.com/th/id/OIP.KNHJ3Zj5fMpWo1Hhs97uDwHaF7?rs=1&pid=ImgDetMain',
+    },
+    {
+      'id': '3',
+      'name': 'Bread',
+      'description': 'Brown Bread, Tasty',
+      'price': 2.49,
+      'imageUrl':
+          'https://th.bing.com/th/id/R.92f8ee7be77d7b53d5304093b3861e18?rik=VJ9Fr5q6neawOQ&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2f7%2f71%2fAnadama_bread_(1).jpg&ehk=8o8tB4WgouX4ktS06icsn57N2DKLcg95T9oUA%2b%2fSmQs%3d&risl=1&pid=ImgRaw&r=0',
+    },
+    {
+      'id': '4',
+      'name': 'Brown Eggs',
+      'description': 'Farm-fresh brown eggs',
+      'price': 3.99,
+      'imageUrl':
+          'https://th.bing.com/th/id/OIP.OLfcOSyImw2f9W4A3fboxgAAAA?rs=1&pid=ImgDetMain',
+    },
+  ];
+}

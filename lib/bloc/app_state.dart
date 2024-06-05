d9848e41099a6e0ac9bb143e097165d5426bd2e9
dinod tharinda <dinodtharinda2001@gmail.com>
2024-06-05 14:11:07 +0530

    restart

--- lib/bloc/app_state.dart
+++ lib/bloc/app_state.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_state_flutter/models.dart';
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 
 @immutable
 class AppState {
@@ -30,4 +31,31 @@ class AppState {
       'fetchedNotes': fetchedNotes,
     }.toString();
   }
+
+  @override
+  bool operator ==(covariant AppState other) {
+    final otherPropertiesAreEqual = isLoading == other.isLoading &&
+        loginHandle == other.loginHandle &&
+        loginError == other.loginError;
+
+    if (fetchedNotes == null && other.fetchedNotes == null) {
+      return otherPropertiesAreEqual;
+    } else {
+      return otherPropertiesAreEqual &&
+          (fetchedNotes?.isEqualTo(other.fetchedNotes) ?? true);
+    }
+  }
+
+  @override
+  int get hashCode => Object.hash(
+        isLoading,
+        loginError,
+        loginHandle,
+        fetchedNotes,
+      );
+}
+
+extension UnordredEquality on Object {
+  bool isEqualTo(other) =>
+      const DeepCollectionEquality.unordered().equals(this, other);
 }

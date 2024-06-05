85753a76523c44da8e7332c29be334732e463dce
dinod tharinda <dinodtharinda2001@gmail.com>
2024-06-06 00:16:01 +0530

    create secodary blocs for multiprovider

---
+++ lib/bloc/bottom_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_state_flutter/bloc/app_bloc.dart';
+
+class BottomBloc extends AppBloc {
+  BottomBloc({
+    required super.urls,
+    required super.waitBeforeLoading,
+  });
+}
---
+++ lib/bloc/top_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_state_flutter/bloc/app_bloc.dart';
+
+class TopBloc extends AppBloc {
+  TopBloc({
+    required super.urls,
+    required super.waitBeforeLoading,
+  });
+}

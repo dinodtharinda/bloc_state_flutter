6f7ffa1e69fecdcbab9acb89e466251cc4a9a5df
dinod tharinda <dinodtharinda2001@gmail.com>
2024-06-11 22:29:36 +0530

    photo gallery  added

---
+++ lib/views/photo_gallery_view.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:bloc_state_flutter/bloc/app_bloc.dart';
+import 'package:bloc_state_flutter/bloc/app_event.dart';
+import 'package:bloc_state_flutter/bloc/app_state.dart';
+import 'package:bloc_state_flutter/views/main_popup_menu.dart';
+import 'package:bloc_state_flutter/views/storage_image_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:image_picker/image_picker.dart';
+
+class PhotoGalleryView extends HookWidget {
+  const PhotoGalleryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final picker = useMemoized(() => ImagePicker(), [key]);
+    final images = context.watch<AppBloc>().state.images ?? [];
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Photo Gallery'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final image = await picker.pickImage(source: ImageSource.gallery);
+              if (image == null) {
+                return;
+              }
+              context.read<AppBloc>().add(
+                    AppEventUploadImage(filePathToUpload: image.path),
+                  );
+            },
+            icon: const Icon(Icons.upload),
+          ),
+          const MainPopupMenuButton(),
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(8),
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+        children: images
+            .map(
+              (img) => StorageImageView(image: img),
+            )
+            .toList(),
+      ),
+    );
+  }
+}

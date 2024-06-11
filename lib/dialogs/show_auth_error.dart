6389e28de1460fea9857018de020225c2c457c21
dinod tharinda <dinodtharinda2001@gmail.com>
2024-06-11 22:30:22 +0530

    auth error dialog

--- lib/dialogs/show_auth_error.dart
+++ lib/dialogs/show_auth_error.dart
@@ -2,10 +2,10 @@ import 'package:bloc_state_flutter/auth/auth_error.dart';
 import 'package:bloc_state_flutter/dialogs/generic_dialog.dart';
 import 'package:flutter/material.dart';
 
-Future<void> showAuthError(
-  AuthError authError,
-  BuildContext context,
-) {
+Future<void> showAuthError({
+  required AuthError authError,
+  required BuildContext context,
+}) {
   return showGenericDialog<bool>(
     context: context,
     title: authError.dialogTitle,
